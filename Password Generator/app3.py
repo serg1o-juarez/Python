e5c4e5c13485d667a73dbd41e18268d7760f69a8
serg1o-juarez <162925334+serg1o-juarez@users.noreply.github.com>
2024-03-10 17:50:04 -0600

    Create app3.py

---
+++ Password Generator/app3.py
@@ -0,0 +1,13 @@
+import secrets
+import string
+
+def password_generator(password_length):
+  characters = string.ascii_letters + string.digits
+  secure_password = ' '.join(secrets.choice(characters) for i in range(password_length))
+  return secure_password
+
+def main():
+  user_password_length = int(input("Input number of digits for password: "))
+  print("Password: ", password_generator(user_password_length))
+
+main()
